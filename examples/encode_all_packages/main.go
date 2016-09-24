28ba856c1d3717aadf7a87ccc0ce8570b28c4547
Benjamin Radovsky <radovskyb@gmail.com>
2016-09-24 20:44:35 +1000

    .

--- examples/encode_all_packages/main.go
+++ examples/encode_all_packages/main.go
@@ -1,13 +1,3 @@
-# inspect
-`inspect` is a small Go package that makes it easy to inspect information about `Go` source code.
-
-`inspect` wraps functionality from standard `Go` packages such as `go/ast`, `go/parser` and `go/token`, to make it easy to inspect information about `Go` source code.
-
-## Examples: 
-
-#### Encode all standard library package's information into a single json file.
-
-```go
 package main
 
 import (
@@ -31,22 +21,28 @@ func main() {
 	// Get the path of the root of the Go standard library packages.
 	pkgsRoot := filepath.Join(strings.TrimSuffix(gobin, filepath.Join("bin", "go")), "src")
 
-	// Get all packages.
+	// Parse all Go packages.
 	pkgs, err := inspect.ParsePackagesFromDir(pkgsRoot, true)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// Delete any non-library, main package's.
 	delete(pkgs, "main")
 
-	jsonFile, err := os.Create("functions.json")
+	// Create a new json file to store all of Go's standard package library info.
+	jsonFile, err := os.Create("packages.json")
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	// Create a new json encoder that writes to jsonFile and set it's
+	// indentation formatting to a single tab.
 	encoder := json.NewEncoder(jsonFile)
 	encoder.SetIndent("", "\t")
+
+	// Encode all of the Package's to the json file.
 	if err := encoder.Encode(pkgs); err != nil {
 		log.Fatalln(err)
 	}
 }
-```
